e7ed44c9b8f99914993ad5d79c5653cc95138d93
rudra1101 <rudra.shankar@nusummit.com>
2026-08-20 16:01:51 +0530

    Wire authentication provider into frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,20 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { AuthProvider } from "./auth/AuthContext";
+import { installAuthenticatedFetch } from "./auth/installAuthenticatedFetch";
 import theme from "./theme/theme";
+
+installAuthenticatedFetch();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
