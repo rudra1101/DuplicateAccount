343ca9fdfed314b1113981d2c8e0d8b2aacaad86
rudra1101 <rudra.shankar@nusummit.com>
2026-08-20 17:03:19 +0530

    Use current frontend host for role management API

--- frontend/src/services/roleService.ts
+++ frontend/src/services/roleService.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = `${window.location.protocol}//${window.location.hostname}:8000/api`;
 
 export interface PermissionItem {
   id: number;
